618a00464ef5410282f85610fdab7448b454874c
ChaitraL99 <chaitra.lakshminarayan99@gmail.com>
2020-07-23 22:54:21 +0530

    Updates

---
+++ precache-manifest.833a0dd9c851cf6b13400c04459e6060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82fc5bb3ea6c5ff2881ae09e5519d29",
+    "url": "/uiToDisplayJsonData/index.html"
+  },
+  {
+    "revision": "993957145a16cf0c929e",
+    "url": "/uiToDisplayJsonData/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "dddf56aeb3905ef085f9",
+    "url": "/uiToDisplayJsonData/static/css/main.ee50d2d7.chunk.css"
+  },
+  {
+    "revision": "993957145a16cf0c929e",
+    "url": "/uiToDisplayJsonData/static/js/2.db07e8bf.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/uiToDisplayJsonData/static/js/2.db07e8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddf56aeb3905ef085f9",
+    "url": "/uiToDisplayJsonData/static/js/main.f5854d99.chunk.js"
+  },
+  {
+    "revision": "2833936cf3ead45b470d",
+    "url": "/uiToDisplayJsonData/static/js/runtime-main.b5ecbf74.js"
+  }
+]);
